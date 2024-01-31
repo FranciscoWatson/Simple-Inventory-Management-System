b734c0d2fbe587b306f56021e7ce5dffbcde83bb
Francisco Watson <francisco@watson.com.ar>
2024-01-31 17:04:08 -0300

    Add Program's Main Menu for the user to choose an option

--- Simple-Inventory-Managment-System/Program.cs
+++ Simple-Inventory-Managment-System/Program.cs
@@ -2,15 +2,54 @@
 
 using Simple_Inventory_Managment_System;
 
-Inventory inventory = new Inventory();
 
-Console.Write("Enter product name: ");
-string productName = Console.ReadLine();
-Console.Write("Enter product price: ");
-double productPrice = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter product quantity: ");
-int productQuantity = Convert.ToInt32(Console.ReadLine());
+namespace Simple_Inventory_Management_System
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Inventory inventory = new Inventory();
 
-Product newProduct = new Product(productName, productPrice, productQuantity);
+            bool menu = true;
 
-inventory.AddProduct(newProduct);
+            while (menu)
+            {
+                Console.WriteLine("\nInventory Management System");
+                Console.WriteLine("1. Add a product");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter an option (1-1): ");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        AddProduct(inventory);
+                        break;
+                    case "0":
+                        menu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice option");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Exiting Program...");
+        }
+
+        static void AddProduct(Inventory inventory) 
+        {
+            Console.Write("Enter product name: ");
+            string productName = Console.ReadLine();
+            Console.Write("Enter product price: ");
+            double productPrice = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter product quantity: ");
+            int productQuantity = Convert.ToInt32(Console.ReadLine());
+            Product newProduct = new Product(productName, productPrice, productQuantity);
+            inventory.AddProduct(newProduct);
+        }
+    }
+}
+
