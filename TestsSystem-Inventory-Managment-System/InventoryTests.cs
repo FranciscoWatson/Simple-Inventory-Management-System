25df9c7a61aa99e9466c544e290c974502788177
Francisco Watson <francisco@watson.com.ar>
2024-02-01 11:20:02 -0300

    Add Tests

---
+++ TestsSystem-Inventory-Managment-System/InventoryTests.cs
@@ -0,0 +1,31 @@
+
+using Simple_Inventory_Managment_System;
+
+namespace TestsSystem_Inventory_Managment_System
+{
+    public class InventoryTests
+    {
+        [Fact]
+        public void AddAProductShouldMakeTheProductsListBigger()
+        {
+            Inventory inventory = new Inventory();
+            Product product = new Product("Iphone 15", 1000, 2);
+
+            inventory.AddProduct(product);
+
+            Assert.Equal(1, inventory.products.Count);
+        }
+        [Fact]
+        public void AddMultipleProductShouldMakeTheProductsListBigger()
+        {
+            Inventory inventory = new Inventory();
+            Product product = new Product("Iphone 15", 1000, 2);
+            Product product2 = new Product("Iphone 16", 500, 7);
+
+            inventory.AddProduct(product);
+            inventory.AddProduct(product2);
+
+            Assert.Equal(2, inventory.products.Count);
+        }
+    }
+}
