006206e3feeda0dfa1104977a7fce1bbfa59d73c
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-09 15:14:05 -0300

    Change Behaviour to EditProduct to Do it to the database

--- Simple-Inventory-Managment-System/ProductRepository.cs
+++ Simple-Inventory-Managment-System/ProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace Simple_Inventory_Managment_System;
 
@@ -52,48 +53,94 @@ public class ProductRepository
         
     
 
-    public void EditProduct(string name)
+    public void EditProduct(string productName)
     {
-        Product productToUpdate = Products.Find(product => product.Name == name);
-            if (productToUpdate != null)
+        Product productToUpdate = null;
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+
+            string query = $"SELECT * FROM Products WHERE Products.Name = @productName";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
-                Console.WriteLine("What field do you wish to change?");
-                Console.WriteLine("1- Name\n2- Price\n3- Quantity \n");
-                Console.Write("Enter an option (1-3): ");
-                string input = Console.ReadLine();
+                command.Parameters.AddWithValue("@productName", productName);
 
-                if (int.TryParse(input, out int option))
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                  
-                    switch (option)
+                    while (reader.Read())
                     {
-                        case 1:
-                            Console.WriteLine("Write the new name for the product: ");
-                            string newName = Console.ReadLine();
-                            productToUpdate.Name = newName;                           
-                            break;
-                        case 2:
-                            Console.WriteLine("Write the new price for the product: ");
-                            string newPrice = Console.ReadLine();
-                            productToUpdate.Price = decimal.Parse(newPrice, System.Globalization.CultureInfo.InvariantCulture);                          
-                            break;
-                        case 3:
-                            Console.WriteLine("Write the new quantity for the product: ");
-                            string newQuantity = Console.ReadLine();
-                            productToUpdate.Quantity = int.Parse(newQuantity, System.Globalization.CultureInfo.InvariantCulture);                            
-                            break;
-                        default:
-                            Console.WriteLine("Invalid choice option");
-                            return;
+
+                        int productId = (int)reader["ProductID"];
+                        string name = reader["Name"] != DBNull.Value ? (string)reader["Name"] : null;
+                        decimal price = (decimal)reader["Price"];
+                        int quantity = (int)reader["Quantity"];
+
+                        productToUpdate = new Product(productId, name, price, quantity);
                     }
-                    Console.WriteLine($"Product updated to --> Name: {productToUpdate.Name}, Price: {productToUpdate.Price}, Quantity: {productToUpdate.Quantity}");
                 }
-                else Console.WriteLine("Invalid choice option");
-
-
-                
             }
-            else Console.WriteLine("Product not found");
+        }
+        if (productToUpdate != null)
+        {
+            Console.WriteLine("What field do you wish to change?");
+            Console.WriteLine("1- Name\n2- Price\n3- Quantity \n");
+            Console.Write("Enter an option (1-3): ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int option))
+            {
+              
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Write the new name for the product: ");
+                        string newName = Console.ReadLine();
+                        productToUpdate.Name = newName;                           
+                        break;
+                    case 2:
+                        Console.WriteLine("Write the new price for the product: ");
+                        string newPrice = Console.ReadLine();
+                        productToUpdate.Price = decimal.Parse(newPrice, System.Globalization.CultureInfo.InvariantCulture);                          
+                        break;
+                    case 3:
+                        Console.WriteLine("Write the new quantity for the product: ");
+                        string newQuantity = Console.ReadLine();
+                        productToUpdate.Quantity = int.Parse(newQuantity, System.Globalization.CultureInfo.InvariantCulture);                            
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice option");
+                        return;
+                }
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = $"UPDATE Products SET Name = @Name, Price = @Price, Quantity = @Quantity WHERE ProductID = @ProductId";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("Name", productToUpdate.Name);
+                    command.Parameters.AddWithValue("Price", productToUpdate.Price);
+                    command.Parameters.AddWithValue("Quantity", productToUpdate.Quantity);
+                    command.Parameters.AddWithValue("ProductId", productToUpdate.ProductId);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+
+
+
+            Console.WriteLine($"Product updated to --> Name: {productToUpdate.Name}, Price: {productToUpdate.Price}, Quantity: {productToUpdate.Quantity}");
+            }
+            else Console.WriteLine("Invalid choice option");
+
+
+            
+        }
+        else Console.WriteLine("Product not found");
     }
 
     public void DeleteProduct(string productName)
@@ -135,6 +182,7 @@ public class ProductRepository
                 }
             }
         }
+
         Console.WriteLine($"ProductId: {product.ProductId}, Name: {product.Name}, Price: {product.Price}, Quantity: {product.Quantity}");
 
     }
